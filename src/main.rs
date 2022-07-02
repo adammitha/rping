983ef89ca15239f9262bd64532cc75dc4204c7cd
Adam Mitha <adam.mitha@gmail.com>
2022-07-01 17:59:56 -0700

    Parse command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::net::IpAddr;
+
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let host: Host = args.host.into();
+
+    println!("Host: {:?}", host);
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    #[clap(value_parser)]
+    host: String,
+}
+
+#[derive(Debug)]
+enum Host {
+    Ip(IpAddr),
+    Hostname(String),
+}
+
+impl From<String> for Host {
+    fn from(host: String) -> Self {
+        match host.parse::<IpAddr>() {
+            Ok(addr) => Host::Ip(addr),
+            Err(_) => Host::Hostname(host),
+        }
+    }
 }
