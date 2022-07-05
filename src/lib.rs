245d38589675d32d9a1b7fcc15e11910fa99d7e0
Adam Mitha <adam.mitha@gmail.com>
2022-07-04 18:36:13 -0700

    Define an RPing struct to expose to CLI

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,20 @@
+#![allow(dead_code)]
 mod raw_socket;
+
+use std::io::Result;
+
+use raw_socket::RawSocket;
+
+pub struct RPing {
+    socket: RawSocket,
+    timeout: u32,
+}
+
+impl RPing {
+    pub fn new(timeout: u32) -> Result<Self> {
+        Ok(Self {
+            socket: RawSocket::new()?,
+            timeout,
+        })
+    }
+}
