85c707852f5e2188155d846dfd57b93448ef8e6c
Adam Mitha <adam.mitha@gmail.com>
2022-07-04 18:35:12 -0700

    RawSocket constructor

--- src/raw_socket.rs
+++ src/raw_socket.rs
@@ -1,5 +1,23 @@
+use std::io::{Error, Result};
+
 use libc::c_int;
 
+/// RawSocket is a safe wrapper around a Linux `raw(7)` socket
 pub struct RawSocket {
     inner: c_int,
+}
+
+impl RawSocket {
+    pub fn new() -> Result<Self> {
+        let sock_fd =
+            check_err(unsafe { libc::socket(libc::AF_INET, libc::SOCK_RAW, libc::IPPROTO_ICMP) })?;
+        Ok(Self { inner: sock_fd })
+    }
+}
+
+fn check_err(return_code: libc::c_int) -> Result<libc::c_int> {
+    if return_code < 0 {
+        return Err(Error::last_os_error());
+    }
+    Ok(return_code)
 }
