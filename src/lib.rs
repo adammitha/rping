d62d22d2cd34de555b24d5b89b0570c837e7f4a1
Adam Mitha <adam.mitha@gmail.com>
2022-11-08 11:57:59 -0800

    Update clap and resolve hostname into an address.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,37 @@
 #![allow(dead_code)]
-mod raw_socket;
 mod icmp;
+mod raw_socket;
 
 use std::io::Result;
+use std::net::SocketAddr;
+use std::net::SocketAddrV4;
+use std::net::ToSocketAddrs;
 
 use raw_socket::RawSocket;
 
-
 pub struct RPing {
     socket: RawSocket,
+    pub host: SocketAddrV4,
 }
 
 impl RPing {
-    pub fn new(timeout: i64) -> Result<Self> {
+    pub fn new(timeout: i64, host: impl ToSocketAddrs) -> Result<Self> {
+        let resolved_host: SocketAddrV4 = match host
+            .to_socket_addrs()?
+            .filter(|a| {
+                matches!(a, SocketAddr::V4(_))
+            })
+            .next()
+        {
+            Some(SocketAddr::V4(addr)) => Ok(addr),
+            _ => Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Failed to resolve the host",
+            )),
+        }?;
         Ok(Self {
             socket: RawSocket::with_timeout(timeout)?,
+            host: resolved_host,
         })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,19 @@
-use std::net::Ipv4Addr;
-
+use rping::RPing;
 use clap::Parser;
 
 fn main() {
     let args = Args::parse();
-
-    let host: Host = args.host.into();
-
-    println!("Host: {:?}, Timeout: {:?}s", host, args.timeout);
+    println!("Resolving host {}", args.host);
+    let rping = RPing::new(args.timeout, (args.host.clone(), 0u16)).unwrap();
+    println!("Pinging host: {}", rping.host.ip());
 }
 
 #[derive(Parser, Debug)]
-#[clap(version, about, long_about = None)]
+#[command(version, about, long_about = None)]
 struct Args {
-    #[clap(
-        value_parser,
-        help = "Host machine to ping. May be an IPv4 address or domain name."
-    )]
+    /// Host machine to ping. May be an IPv4 address or domain name.
     host: String,
-    #[clap(short, long, default_value = "1", help = "Timeout interval (seconds)")]
-    timeout: u32,
-}
-
-#[derive(Debug)]
-enum Host {
-    Ip(Ipv4Addr),
-    Hostname(String),
-}
-
-impl From<String> for Host {
-    fn from(host: String) -> Self {
-        match host.parse::<Ipv4Addr>() {
-            Ok(addr) => Host::Ip(addr),
-            Err(_) => Host::Hostname(host),
-        }
-    }
+    #[arg(short, long, default_value = "1")]
+    /// Timeout interval (seconds)
+    timeout: i64,
 }
